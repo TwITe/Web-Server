f2604d44edd5cd50d2cab8141c5370c0334656ae
TwITe <dm.blink19@gmail.com>
2018-01-11 00:10:05 +0300

    delete client http request handler class

---
+++ client_handler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by twite on 08.01.18.
+//
+
+#include "client_handler.h"
---
+++ client_handler.h
@@ -0,0 +1,8 @@
+//
+// Created by twite on 08.01.18.
+//
+
+#ifndef WEB_CLIENT_HANDLER_H
+#define WEB_CLIENT_HANDLER_H
+
+#endif //WEB_CLIENT_HANDLER_H
