cdc9f535ce022a4099f8462c22194502f491cdea
TwITe <dm..blink19@gmail.com>
2018-05-01 15:30:47 +0300

    rename test

--- web_server_tests_clientside.cpp
+++ web_server_tests_clientside.cpp
@@ -2,7 +2,7 @@
 #include "catch.hpp"
 using namespace std;
 
-TEST_CASE("Check base read/send functions","Client") {
+TEST_CASE("Check for getting 404 response because of non-existing web handler for current request","Client") {
     unsigned short int PORT = 8080;
     int sockfd;
     struct sockaddr_in server_address{};
@@ -16,7 +16,7 @@ TEST_CASE("Check base read/send functions","Client") {
         cout << "[Server] Invalid IP address" << endl;
     }
 
-    if (connect(sockfd, (struct sockaddr*) &server_address, sizeof(server_address)) < 0) {
+    if (connect(sockfd, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) {
         cout << "[Client] Connection failed" << endl;
     }
     cout << "[Client] All setting are done" << endl;
@@ -24,18 +24,18 @@ TEST_CASE("Check base read/send functions","Client") {
     cout << "----------------------------" << endl << endl;
 
     string message = "GET /tutorials/other/top-20-mysql-best-practices/ HTTP/1.1\n"
-            "Host: net.tutsplus.com\n"
-            "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)\n"
-            "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8*\n"
-            "Accept-Language: en-us,en;q=0.5\n"
-            "Accept-Encoding: gzip,deflate\n"
-            "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7\n"
-            "Keep-Alive: 300\n"
-            "Connection: keep-alive\n"
-            "Cookie: PHPSESSID=r2t5uvjq435r4q7ib3vtdjq120\n"
-            "Pragma: no-cache\n"
-            "Cache-Control: no-cache\n"
-            "\n";
+                     "Host: net.tutsplus.com\n"
+                     "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)\n"
+                     "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8*\n"
+                     "Accept-Language: en-us,en;q=0.5\n"
+                     "Accept-Encoding: gzip,deflate\n"
+                     "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7\n"
+                     "Keep-Alive: 300\n"
+                     "Connection: keep-alive\n"
+                     "Cookie: PHPSESSID=r2t5uvjq435r4q7ib3vtdjq120\n"
+                     "Pragma: no-cache\n"
+                     "Cache-Control: no-cache\n"
+                     "\n";
     if (write(sockfd, message.c_str(), message.size()) == -1) {
         cout << "[Client] Message sending failed" << endl;
     }
@@ -49,12 +49,12 @@ TEST_CASE("Check base read/send functions","Client") {
     string received_message = string(buffer);
 
     string proper_response = "HTTP/1.1 404 Not Found\n"
-            "Content-Length: 26\n"
-            "\n"
-            "Oops! Something went wrong\n";
+                             "Content-Length: 26\n"
+                             "\n"
+                             "Oops! Something went wrong\n";
 
     REQUIRE(received_message == proper_response);
-////
+
     cout << "[Client] Server message: " << buffer << endl;
     cout << "============================" << endl << endl;
 }
