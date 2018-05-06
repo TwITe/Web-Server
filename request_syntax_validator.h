21e87cd6969f77f8152ddcefe840b566ddb769cd
twite <dm.blink19@gmail.com>
2018-05-07 01:34:01 +0300

    add new class validating request syntaxis

---
+++ request_syntax_validator.h
@@ -0,0 +1,10 @@
+#ifndef WEB_REQUEST_SYNTAX_VALIDATOR_H
+#define WEB_REQUEST_SYNTAX_VALIDATOR_H
+
+namespace webserver {
+    class request_syntax_validator {
+        //TODO: Написать класс по проверке пришедшего request на валидность синтаксиса
+    };
+}
+
+#endif //WEB_REQUEST_SYNTAX_VALIDATOR_H
