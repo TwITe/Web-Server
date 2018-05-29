3fd569929044ff9b083ebc67ca12341dd872d9c6
twite <dm.blink19@gmail.com>
2018-05-29 23:59:26 +0300

    add new tests

--- http_request_parser_tests.cpp
+++ http_request_parser_tests.cpp
@@ -53,21 +53,76 @@ TEST_CASE("ParseRequestBody_RawRequest_ParsedRequestBody", "Parser") {
 
     webserver::http_request proper_request;
 
-    proper_request.set_http_request_method("GET");
-    proper_request.set_http_request_url("net.tutsplus.com/tutorials/other/top-20-mysql-best-practices/");
-    webserver::http_header header;
-    header.type = "Host";
-    header.value = "net.tutsplus.com";
-    proper_request.add_http_request_header(header);
-    header.type = "Content-type";
-    header.value = "utf-8";
-    proper_request.add_http_request_header(header);
     proper_request.set_http_request_body("Hello, World!");
 
-    webserver::http_request received_request;
-
     webserver::http_request_parser parser;
-    received_request = parser.parse_request(raw_request);
+    webserver::http_request received_request = parser.parse_request(raw_request);
 
     REQUIRE(proper_request.get_request_body() == received_request.get_request_body());
+}
+
+TEST_CASE("ParseRequestParameters_RawRequest_ParsedUrlParameters", "Parser") {
+    vector<string> raw_request;
+
+    raw_request.emplace_back("GET /?clid=2063711&clid=23141 HTTP/1.1");
+    raw_request.emplace_back("Host: yandex.ru");
+    raw_request.emplace_back("\r\n");
+
+    webserver::http_request proper_request;
+
+    webserver::request_param request_parameter;
+    request_parameter = {"clid", "2063711"};
+    proper_request.add_http_request_param(request_parameter);
+    request_parameter = {"clid", "23141"};
+    proper_request.add_http_request_param(request_parameter);
+
+    webserver::http_request_parser parser;
+    webserver::http_request received_request = parser.parse_request(raw_request);
+
+    const vector<webserver::request_param> received_request_parameters = received_request.get_request_params();
+    const vector<webserver::request_param> proper_request_parameters = proper_request.get_request_params();
+
+    for (unsigned long current_request_parameter_number = 0; current_request_parameter_number < proper_request_parameters.size(); current_request_parameter_number++) {
+        REQUIRE(received_request_parameters[current_request_parameter_number].name == proper_request_parameters[current_request_parameter_number].name);
+        REQUIRE(received_request_parameters[current_request_parameter_number].value == proper_request_parameters[current_request_parameter_number].value);
+    }
+}
+
+TEST_CASE("ExtendRequestUrlByHost_NonAbsoluteUrl_AbsoluteUrl", "Parser") {
+    vector<string> raw_request;
+
+    raw_request.emplace_back("GET /feed HTTP/1.1");
+    raw_request.emplace_back("Host: vk.com");
+    raw_request.emplace_back("\r\n");
+
+    webserver::http_request proper_request;
+    proper_request.set_http_request_url("vk.com/feed");
+
+    webserver::http_request_parser parser;
+    webserver::http_request received_request = parser.parse_request(raw_request);
+
+    const string& proper_request_url = proper_request.get_request_url();
+    const string& received_request_url = received_request.get_request_url();
+
+    REQUIRE(proper_request_url == received_request_url);
+}
+
+TEST_CASE("ParseRequestLine_RawRequest_ParsedRequestLine") {
+    vector<string> raw_request;
+
+    raw_request.emplace_back("GET /feed HTTP/1.1");
+    raw_request.emplace_back("Host: vk.com");
+    raw_request.emplace_back("\r\n");
+
+    webserver::http_request proper_request;
+    proper_request.set_http_request_url("vk.com/feed");
+    proper_request.set_http_request_method("GET");
+    proper_request.set_http_version("HTTP/1.1");
+
+    webserver::http_request_parser parser;
+    webserver::http_request received_request = parser.parse_request(raw_request);
+
+    REQUIRE(proper_request.get_request_method() == received_request.get_request_method());
+    REQUIRE(proper_request.get_request_url() == received_request.get_request_url());
+    REQUIRE(proper_request.get_request_http_version() == received_request.get_request_http_version());
 }
