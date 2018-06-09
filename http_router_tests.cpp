56a6cf598c7a665f166311ef615454f40b82751b
twite <dm.blink19@gmail.com>
2018-06-09 09:20:35 +0300

    add new tests for http router

--- http_router_tests.cpp
+++ http_router_tests.cpp
@@ -88,7 +88,50 @@ TEST_CASE("GetProperWebHandler_RequestPOSTW/ParametersW/Path_ReceivedPostRequest
     REQUIRE(received_web_handler_pattern == proper_web_handler_pattern);
 }
 
-TEST_CASE("GetProperWebHandler_RequestPOSTW/oParametersW/oPath_ReceivedPostRequestHandler", "HTTP Router") {
+TEST_CASE("GetProperWebHandler_RequestPOSTW/oParametersW/oPathW/Slash_ReceivedPostRequestHandler", "HTTP Router") {
+    vector<webserver::web_handler> handlers;
+
+    _Pragma("GCC diagnostic push")
+    _Pragma("GCC diagnostic ignored \"-Wunused-parameter\"")
+    function<webserver::http_response(webserver::http_request)> index_handler = [&](webserver::http_request request) {
+        webserver::http_response response;
+
+        string response_body = "hello world!";
+
+        response.set_response_body(response_body);
+        response.set_response_http_code(200);
+        response.set_response_length(response_body.size());
+
+        vector<webserver::http_header> headers;
+        headers.push_back(webserver::http_header{"Content-Type", "text/plain"});
+        response.set_response_headers(headers);
+
+        return response;
+    };
+    _Pragma("GCC diagnostic pop")
+
+    webserver::web_handler proper_web_handler("/", "POST", index_handler);
+
+    handlers.emplace_back(proper_web_handler);
+
+    webserver::http_request test_request;
+    test_request.set_http_request_method("POST");
+    test_request.set_http_request_url("https://vk.com/");
+
+    webserver::http_router test_router;
+    webserver::web_handler received_web_handler = test_router.get_suitable_request_handler(handlers, test_request);
+
+    const string& received_web_handler_method = received_web_handler.get_web_handler_method();
+    const string& received_web_handler_pattern = received_web_handler.get_web_handler_pattern();
+
+    const string& proper_web_handler_method = proper_web_handler.get_web_handler_method();
+    const string& proper_web_handler_pattern = proper_web_handler.get_web_handler_pattern();
+
+    REQUIRE(received_web_handler_method == proper_web_handler_method);
+    REQUIRE(received_web_handler_pattern == proper_web_handler_pattern);
+}
+
+TEST_CASE("GetProperWebHandler_RequestPOSTW/oParametersW/oPathW/oSlash_ReceivedPostRequestHandler", "HTTP Router") {
     vector<webserver::web_handler> handlers;
 
     _Pragma("GCC diagnostic push")
@@ -131,7 +174,7 @@ TEST_CASE("GetProperWebHandler_RequestPOSTW/oParametersW/oPath_ReceivedPostReque
     REQUIRE(received_web_handler_pattern == proper_web_handler_pattern);
 }
 
-TEST_CASE("GetProperWebHandler_RequestPOSTW/ParametersW/oPath_ReceivedPostRequestHandler", "HTTP Router") {
+TEST_CASE("GetProperWebHandler_RequestPOSTW/ParametersW/oPathW/Slash_ReceivedPostRequestHandler", "HTTP Router") {
     vector<webserver::web_handler> handlers;
 
     _Pragma("GCC diagnostic push")
@@ -160,6 +203,49 @@ TEST_CASE("GetProperWebHandler_RequestPOSTW/ParametersW/oPath_ReceivedPostReques
     webserver::http_request test_request;
     test_request.set_http_request_method("POST");
     test_request.set_http_request_url("https://www.yandex.ru/?clid=2063711");
+
+    webserver::http_router test_router;
+    webserver::web_handler received_web_handler = test_router.get_suitable_request_handler(handlers, test_request);
+
+    const string& received_web_handler_method = received_web_handler.get_web_handler_method();
+    const string& received_web_handler_pattern = received_web_handler.get_web_handler_pattern();
+
+    const string& proper_web_handler_method = proper_web_handler.get_web_handler_method();
+    const string& proper_web_handler_pattern = proper_web_handler.get_web_handler_pattern();
+
+    REQUIRE(received_web_handler_method == proper_web_handler_method);
+    REQUIRE(received_web_handler_pattern == proper_web_handler_pattern);
+}
+
+TEST_CASE("GetProperWebHandler_RequestPOSTW/ParametersW/oPathW/oSlash_ReceivedPostRequestHandler", "HTTP Router") {
+    vector<webserver::web_handler> handlers;
+
+    _Pragma("GCC diagnostic push")
+    _Pragma("GCC diagnostic ignored \"-Wunused-parameter\"")
+    function<webserver::http_response(webserver::http_request)> index_handler = [&](webserver::http_request request) {
+        webserver::http_response response;
+
+        string response_body = "hello world!";
+
+        response.set_response_body(response_body);
+        response.set_response_http_code(200);
+        response.set_response_length(response_body.size());
+
+        vector<webserver::http_header> headers;
+        headers.push_back(webserver::http_header{"Content-Type", "text/plain"});
+        response.set_response_headers(headers);
+
+        return response;
+    };
+    _Pragma("GCC diagnostic pop")
+
+    webserver::web_handler proper_web_handler("/", "POST", index_handler);
+
+    handlers.emplace_back(proper_web_handler);
+
+    webserver::http_request test_request;
+    test_request.set_http_request_method("POST");
+    test_request.set_http_request_url("https://www.yandex.ru?clid=2063711");
 
     webserver::http_router test_router;
     webserver::web_handler received_web_handler = test_router.get_suitable_request_handler(handlers, test_request);
@@ -211,8 +297,8 @@ TEST_CASE("NonexistentProperWebHandler_RequestGET_ReceiveErrorHandler", "HTTP Ro
     const string& received_web_handler_method = received_web_handler.get_web_handler_method();
     const string& received_web_handler_pattern = received_web_handler.get_web_handler_pattern();
 
-    const function<webserver::http_response(webserver::http_request)>& handler = received_web_handler.get_transform_to_response_function();
-    webserver::http_response received_response = handler(test_request);
+    const function<webserver::http_response(webserver::http_request)>& convert_request_to_response = received_web_handler.get_transform_to_response_function();
+    webserver::http_response received_response = convert_request_to_response(test_request);
     const string& response_body = received_response.get_response_body();
 
     REQUIRE(received_web_handler_method.empty());
