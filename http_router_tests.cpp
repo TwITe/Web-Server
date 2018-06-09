1db0700ce8ae52cb15c33203d4b990af9b212140
twite <dm.blink19@gmail.com>
2018-06-09 09:30:38 +0300

    add new tests for http router

--- http_router_tests.cpp
+++ http_router_tests.cpp
@@ -260,7 +260,7 @@ TEST_CASE("GetProperWebHandler_RequestPOSTW/ParametersW/oPathW/oSlash_ReceivedPo
     REQUIRE(received_web_handler_pattern == proper_web_handler_pattern);
 }
 
-TEST_CASE("NonexistentProperWebHandler_RequestGET_ReceiveErrorHandler", "HTTP Router") {
+TEST_CASE("NonexistentProperWebHandler_RequestGETBadMethod_ReceiveErrorHandler", "HTTP Router") {
     vector<webserver::web_handler> handlers;
 
     _Pragma("GCC diagnostic push")
@@ -289,6 +289,52 @@ TEST_CASE("NonexistentProperWebHandler_RequestGET_ReceiveErrorHandler", "HTTP Ro
     webserver::http_request test_request;
     test_request.set_http_request_method("GET");
     test_request.set_http_request_url("https://vk.com/im");
+
+    webserver::http_router test_router;
+
+    webserver::web_handler received_web_handler = test_router.get_suitable_request_handler(handlers, test_request);
+
+    const string& received_web_handler_method = received_web_handler.get_web_handler_method();
+    const string& received_web_handler_pattern = received_web_handler.get_web_handler_pattern();
+
+    const function<webserver::http_response(webserver::http_request)>& convert_request_to_response = received_web_handler.get_transform_to_response_function();
+    webserver::http_response received_response = convert_request_to_response(test_request);
+    const string& response_body = received_response.get_response_body();
+
+    REQUIRE(received_web_handler_method.empty());
+    REQUIRE(received_web_handler_pattern.empty());
+    REQUIRE(response_body == "Not Found");
+}
+
+TEST_CASE("NonexistentProperWebHandler_RequestGETBadPattern_ReceiveErrorHandler", "HTTP Router") {
+    vector<webserver::web_handler> handlers;
+
+    _Pragma("GCC diagnostic push")
+    _Pragma("GCC diagnostic ignored \"-Wunused-parameter\"")
+    function<webserver::http_response(webserver::http_request)> index_handler = [&](webserver::http_request request) {
+        webserver::http_response response;
+
+        string response_body = "hello world!";
+
+        response.set_response_body(response_body);
+        response.set_response_http_code(200);
+        response.set_response_length(response_body.size());
+
+        vector<webserver::http_header> headers;
+        headers.push_back(webserver::http_header{"Content-Type", "text/plain"});
+        response.set_response_headers(headers);
+
+        return response;
+    };
+    _Pragma("GCC diagnostic pop")
+
+    webserver::web_handler proper_web_handler("/im", "POST", index_handler);
+
+    handlers.emplace_back(proper_web_handler);
+
+    webserver::http_request test_request;
+    test_request.set_http_request_method("POST");
+    test_request.set_http_request_url("https://vk.com/feed");
 
     webserver::http_router test_router;
 
