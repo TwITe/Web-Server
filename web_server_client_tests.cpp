5ca2899faa1690e7f69c30e377873a1945edf54d
twite <dm.blink19@gmail.com>
2018-06-09 11:32:57 +0300

    add new test (200 ok response)

--- web_server_client_tests.cpp
+++ web_server_client_tests.cpp
@@ -2,8 +2,6 @@
 #include "catch.hpp"
 using namespace std;
 
-//TODO: Добавить тест по отправлению пустой строки. Должен прийти 400 (bad request) response
-
 void open_connection(unsigned short int PORT, int& sockfd, struct sockaddr_in& server_address) {
     server_address = {};
 
@@ -59,6 +57,49 @@ TEST_CASE("Get 404 response for request with non-existing web handler","Client")
                              "Content-Length: 9\r\n"
                              "\r\n"
                              "Not Found";
+
+    cout << "[Client] Server message: " << buffer << endl;
+    cout << "============================" << endl << endl;
+
+    REQUIRE(received_message == proper_response);
+}
+
+TEST_CASE("Get 200 response for request with existing web handler","Client") {
+    unsigned short int PORT = 8080;
+    int sockfd;
+    struct sockaddr_in server_address;
+    open_connection(PORT, sockfd, server_address);
+
+    string message = "GET /index HTTP/1.1\r\n"
+                     "Host: net.tutsplus.com\r\n"
+                     "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)\r\n"
+                     "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8*\r\n"
+                     "Accept-Language: en-us,en;q=0.5\r\n"
+                     "Accept-Encoding: gzip,deflate\r\n"
+                     "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7\r\n"
+                     "Keep-Alive: 300\r\n"
+                     "Connection: keep-alive\n"
+                     "Cookie: PHPSESSID=r2t5uvjq435r4q7ib3vtdjq120\r\n"
+                     "Pragma: no-cache\r\n"
+                     "Cache-Control: no-cache\r\n"
+                     "\r\n";
+    if (write(sockfd, message.c_str(), message.size()) == -1) {
+        cout << "[Client] Message sending failed" << endl;
+    }
+    cout << "[Client] Message sent to server" << endl;
+
+    char buffer[128000];
+
+    memset(&buffer, 0, 128000);
+    read(sockfd, buffer, 128000);
+
+    string received_message = string(buffer);
+
+    string proper_response = "HTTP/1.1 200 OK\r\n"
+                             "Content-Type: text/plain\r\n"
+                             "Content-Length: 13\r\n"
+                             "\r\n"
+                             "Hello, World!";
 
     cout << "[Client] Server message: " << buffer << endl;
     cout << "============================" << endl << endl;
