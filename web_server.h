5dee04d4c4a66c15a501fa529bba471fc715e92f
TwITe <dm.blink19@gmail.com>
2018-07-15 12:02:35 +0300

    code inspecting

--- web_server.h
+++ web_server.h
@@ -77,7 +77,7 @@ namespace webserver {
 
                 //if body exists
                 if (!headers["content-length"].empty()) {
-                    long body_size = stol(headers["content-length"]);
+                    unsigned long body_size = stol(headers["content-length"]);
 
                     if (client_message.size() - index_end_of_headers != body_size) {
                         return false;
